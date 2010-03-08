3cddf210e0f843432550b56569868df79a184e08
Pinhook <contact@pinhooklabs.com>
2010-03-08 13:24:26 -0600

    scaffolding works mostly

--- generate/parts/convert.js
+++ generate/parts/convert.js
@@ -1,3 +1,7 @@
+/**
+ * Converts name into a bunch of useful other parts
+ * @param {Object} name
+ */
 $convert = function(name){
     var className = name.match(/[^\.]*$/)[0] //Customer
 	return {
@@ -6,6 +10,6 @@ $convert = function(name){
         name : name,
 		fullName : name,
 		className : className,
-		plural: steal.Inflector.pluralize(className)
+		plural: steal.Inflector.pluralize(Generate.underscore(className))
     }
 }
--- generate/parts/model.js
+++ generate/parts/model.js
@@ -20,22 +20,6 @@
 		    .pop().push('test').push('qunit').folder()
 				.render(md.underscore+"_test.js","model_unit_test.ejs")
 	}
-	/*
-	
-	
-    //add model and fixtures
-    if($modelType){
-		md.type =  $modelType;
-		var p = jQuery.String.underscore( md.type );
-		Generate.render("models/" + md.underscore+".js", "../generate/templates/"+p+"_model.ejs", md);
-		Generate.render("test/fixtures/" + md.plural+".json.get", "../generate/templates/fixture.ejs", md);
-	}else{
-		
-		Generate.render("models/" + md.underscore+".js", "../generate/templates/model.ejs", md);
-		Generate.render("test/fixtures/" + md.plural+".get", "../generate/templates/fixture.ejs", md);
-	}
-	//add unit tests
-    Generate.render("test/unit/" + md.underscore+"_test.js", "../generate/templates/model_unit_test.ejs", md);*/
 })()
 
 
