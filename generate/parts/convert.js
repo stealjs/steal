5193107269ddd7f74f9b31b7677e35c92c946bc2
Brian Moschel <brian.moschel@gmail.com>
2010-03-08 22:34:37 -0600

    scaffold for functional tests

--- generate/parts/convert.js
+++ generate/parts/convert.js
@@ -4,12 +4,14 @@
  */
 $convert = function(name){
     var className = name.match(/[^\.]*$/)[0] //Customer
+    var appName = name.split(".")[0] //Customer
 	return {
         underscore : Generate.underscore(className),
 		path : Generate.underscore(name).replace(/\./g,"/").replace(/\/[^\/]*$/,""),
         name : name,
 		fullName : name,
 		className : className,
-		plural: steal.Inflector.pluralize(Generate.underscore(className))
+		plural: steal.Inflector.pluralize(Generate.underscore(className)),
+		appName: appName.toLowerCase()
     }
 }
