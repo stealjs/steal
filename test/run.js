f3f21db789bf746d8acbc9cf99234c64c5991608
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-04-02 18:06:45 +0100

    Moved steal qunit test code from big_test.js to steal/test/run.js.

---
+++ test/run.js
@@ -0,0 +1,11 @@
+// load('steal/test/run.js')
+
+/**
+ * Steal qunit tests
+ */
+
+load('steal/test/helpers.js')
+_S.clear();
+
+_S.open("steal/test/qunit/qunit.html");
+//_S.wait("window.jQuery.Test.Unit.complete");
