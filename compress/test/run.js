95a1977342fa7ba511556d62bd476f6ab0cfbeee
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-04-06 00:15:26 +0100

    Added foreign character compression test.

--- compress/test/run.js
+++ compress/test/run.js
@@ -27,3 +27,20 @@ _S.equals(BasicSource, 7, "Basic source not right number")
 _S.clear();
 
 _S.remove('steal/compress/test/production.js')
+
+print("-- Foreign characters --");
+load("steal/compress/compress.js");
+new steal.Compress('steal/compress/test/foreign.html','steal/compress/test');
+_S.clear();
+
+load("steal/compress/test/foreignproduction.js")
+//check that srcs are equal
+f1 = readFile('foreign.js').replace(/\r/,"");
+f2 = readFile('foreignproduction.js');
+if(f1 !=  f2){
+    print(f1+"\n---------------------------\n"+f2);
+    throw "Foreign characters aren't right";
+}
+
+_S.clear();
+_S.remove('steal/compress/test/foreignproduction.js')
