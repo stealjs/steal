36005b64926dd01121c6ef1c67443be7af1a95f6
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-04-06 22:50:32 +0100

    Added big_test.js tests for controller and model generators.

--- generate/test/run.js
+++ generate/test/run.js
@@ -8,13 +8,34 @@
 load('steal/test/helpers.js')
 _S.clear();
 
+print("-- generate cnu plugin --");
 _args = ['cnu']; load('steal/generate/plugin');_S.clear();
 _S.open('cnu/cnu.html')
 if(typeof steal == 'undefined') throw "didn't load steal"
 _S.clear();
 
-//try 2 levels deep
+print("-- try 2 levels deep --");
 _args = ['cnu/widget']; load('steal/generate/plugin');_S.clear();
 _S.open('cnu/widget/widget.html')
 if(typeof steal == 'undefined') throw "didn't load steal"
+_S.clear();
+
+print("-- generate controller --");
+_args = ['Cnu.Controllers.Todos']; load('steal/generate/controller');_S.clear();
+_args = ['Cnu.Models.Todo']; load('steal/generate/model');_S.clear();
+load('steal/file/file.js');
+cnuContent = readFile('cnu/cnu.js').
+    replace(".models()", ".models('todo')").
+    replace(".controllers()", ".controllers('todos')");
+new steal.File('cnu/cnu.js').save( cnuContent );
+/*print("-- unit test --");
+_args = ['Truth']; load('jmvc/generate/unit_test');clearEverything();
+print("-- functional test --");
+_args = ['TruthFunctional']; load('jmvc/generate/functional_test');clearEverything();*/
+print("-- generate page --")
+_args = ['cnugen','cnu']; load('steal/generate/page');_S.clear();
+
+_S.open('cnu/cnugen.html')
+if(typeof Cnu.Controllers.TodosController == 'undefined') throw "didn't load Cnu.Controllers.TodosController"
+if(typeof Cnu.Models.Todo == 'undefined') throw "didn't load Cnu.Models.Todo"
 _S.clear();
