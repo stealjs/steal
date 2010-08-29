a4a621fa3a26c10baf6de1737ee11143151139ec
Pinhook <contact@pinhooklabs.com>
2010-08-28 20:11:21 -0500

    fixing a few minor things

--- build/apps/test.js
+++ build/apps/test.js
@@ -1,8 +1,4 @@
 // load("steal/build/apps/test.js")
 
-load("steal/rhino/steal.js");
-steal.plugins('steal/build','steal/build/scripts','steal/build/styles','steal/build/apps',function(){
-	
-	steal.build.apps(['phui/combobox','phui/modal'])
-	
-})
+_args = ['phui/combobox','phui/modal'];
+load("steal/buildjs");
