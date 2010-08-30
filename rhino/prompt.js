213dfb6c66ca790cc17118ea1b7338c853452dae
Brian Moschel <brian.moschel@gmail.com>
2010-08-30 10:38:37 -0500

    adding support for handling arguments

--- rhino/prompt.js
+++ rhino/prompt.js
@@ -22,5 +22,28 @@ steal(function(steal){
 	    }
 	    return response.match(/[yn]/i)[0].toLowerCase() == "y";
 	}
+	
+	/**
+	 * Accepts an array of possible arguments and creates global variables for each that is found in args
+	 * ie: steal.handleArgs(_args, ["path"])
+	 * Args are passed in via command line scripts like this:
+	 * js run.js path=/one/two docsLocation=docs
+	 * @param {Object} possibleArgs
+	 */
+	steal.handleArgs = function(args, possibleArgs){
+		var i, arg, j, possibleArg, matchedArg, results = {};
+		for(i = 0; i<args.length; i++){
+			arg = args[i];
+			for (j = 0; j < possibleArgs.length; j++) {
+				possibleArg = possibleArgs[j];
+				reg = new RegExp("^"+possibleArg+"\=([^\\s]+)");
+				matchedArg = arg.match(reg);
+				if(matchedArg && matchedArg[1]) {
+					results[possibleArg] = matchedArg[1];
+				}
+			}
+		}
+		return results;
+	}
 })
 
