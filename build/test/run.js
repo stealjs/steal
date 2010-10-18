ca2f46037905a4c69ba6f327a6009886ee9249ff
Brian Moschel <brian.moschel@gmail.com>
2010-10-18 02:00:46 -0400

    fixing steal tests

--- build/test/run.js
+++ build/test/run.js
@@ -9,48 +9,54 @@ steal('//steal/test/test', function( s ) {
 	s.test.clear();
 
 	load('steal/rhino/steal.js')
-	steal("//steal/compress/compress")
+	steal("//steal/build/build")
+	steal("//steal/build/scripts/scripts")
 
-	steal.compress("steal/compress/test/basicpage.html", {
-		output: 'steal/compress/test'
+	steal.build("steal/build/test/basicpage.html", {
+		to: 'steal/build/test'
 	})
-
 	s.test.clear();
 
-	load("steal/compress/test/basicproduction.js")
+	load("steal/build/test/basicproduction.js")
 	s.test.equals(BasicSource, 6, "Basic source not right number")
 
 
+	s.test.clear();
+	s.test.remove('steal/build/test/basicproduction.js')
+	
+	print("-- Compress page using steal --");
+	load('steal/rhino/steal.js')
+	steal("//steal/build/build")
+	steal("//steal/build/scripts/scripts")
+	steal.build("steal/build/test/stealpage.html", {
+		to: 'steal/build/test'
+	})
+	s.test.clear();
+
+	s.test.open('steal/build/test/stealprodpage.html')
+	s.test.equals(BasicSource, 7, "Basic source not right number")
+	s.test.clear();
+
+	s.test.remove('steal/build/test/production.js')
+
+	print("-- Compress page with foreign characters --");
+	load('steal/rhino/steal.js')
+	steal("//steal/build/build")
+	steal("//steal/build/scripts/scripts")
+	steal.build("steal/build/test/foreign.html", {
+		to: 'steal/build/test'
+	})
+	s.test.clear();
+
+	//check that srcs are equal
+	f1 = readFile('foreign.js').replace(/\r/,"");
+	f2 = readFile('foreignproduction.js');
+	if(f1 !=  f2){
+	    print(f1+"\n---------------------------\n"+f2);
+	    throw "Foreign characters aren't right";
+	}
+
+	s.test.clear();
+	s.test.remove('steal/build/test/foreignproduction.js')
 
 });
-/*
-_S.clear();
-_S.remove('steal/compress/test/basicproduction.js')
-
-print("-- Compress page using steal --");
-load("steal/compress/compress.js")
-new steal.Compress(['steal/compress/test/stealpage.html','steal/compress/test']);
-_S.clear();
-
-_S.open('steal/compress/test/stealprodpage.html')
-_S.equals(BasicSource, 7, "Basic source not right number")
-_S.clear();
-
-_S.remove('steal/compress/test/production.js')
-
-print("-- Compress page with foreign characters --");
-load("steal/compress/compress.js");
-new steal.Compress(['steal/compress/test/foreign.html','steal/compress/test']);
-_S.clear();
-
-//load("steal/compress/test/foreignproduction.js")
-//check that srcs are equal
-f1 = readFile('foreign.js').replace(/\r/,"");
-f2 = readFile('foreignproduction.js');
-if(f1 !=  f2){
-    print(f1+"\n---------------------------\n"+f2);
-    throw "Foreign characters aren't right";
-}
-
-_S.clear();
-_S.remove('steal/compress/test/foreignproduction.js')*/
