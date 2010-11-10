2c55c4ed848fce98b12c072bd4f8d4754c4b93a5
Daniel Lamb <daniellmb@hotmail.com>
2010-11-10 14:02:35 -0800

    removed css min hard code optimized the code to minify only once after all the css it combined together and added calculated savings message to the build process. "Nice! Compressed: 22.3% Before 54.3 kb  After: 42.2 kb"

--- build/styles/styles.js
+++ build/styles/styles.js
@@ -1,84 +1,77 @@
-steal(function(steal){
-	
+load("steal/build/styles/cssmin.js");
+steal(function( steal ) {
+
 	/**
 	 * Builds and compresses CSS files.
 	 * @param {Object} opener
 	 * @param {Object} options
 	 */
-	var styles = (steal.build.builders.styles = function(opener, options){
-		print("\nBUILDING STYLES --------------- ");
-			//where we are putting stuff
-		var folder = options.to.substr(0, options.to.length -1),
+	var styles = (steal.build.builders.styles = function( opener, options ) {
+		steal.print("\nBUILDING STYLES --------------- ");
+		//where we are putting stuff
+		var folder = options.to.substr(0, options.to.length - 1),
 			//where the page is
 			pageFolder = steal.File(opener.url).dir(),
 			currentPackage = [];
-		
-		opener.each('link',function(link, text, i){
-			print(link.type)
+
+		opener.each('link', function( link, text, i ) {
+			steal.print(link.type)
 			//let people know we are adding it
-			if(link.href && steal.build.types[link.type]){
-				print(link.href)
-				
+			if ( link.href && steal.build.types[link.type] ) {
+				steal.print(link.href)
+
 				var loc = steal.File(pageFolder).join(link.href),
 					converted = convert(text, loc, folder)
-
-
-				currentPackage.push( steal.cssMin(converted) )
-				
+					currentPackage.push(converted)
 			}
-	
 		});
-		print("")
-		if(currentPackage.length){
-			print("STYLE BUNDLE > "+folder+"/production.css\n")
-			steal.File(folder+"/production.css").save(currentPackage.join(''));
-		}else{
-			print("no styles\n")
+
+		steal.print("")
+		if ( currentPackage.length ) {
+			steal.print("STYLE BUNDLE > " + folder + "/production.css")
+            //now that we have all the css minify and save it
+            var raw_css = currentPackage.join("");
+            var minified_css = steal.cssMin(raw_css);
+            steal.print("Nice! "+calcSavings(raw_css.length,minified_css.length));
+            steal.File(folder + "/production.css").save(minified_css);
+		} else {
+			steal.print("no styles\n")
 		}
-		
-		
-		
 	});
+
 	//used to convert css referencs in one file so they will make sense from prodLocation
-	var convert = function(css, cssLocation, prodLocation){
+	var convert = function( css, cssLocation, prodLocation ) {
 		//how do we go from prod to css
-		 
 		var cssLoc = new steal.File(cssLocation).dir(),
-			newCSss = css.replace(/url\(([^\)]*)\)/g, function(whole, part){
-			
-			//check if url is relative
-			if(! isRelative(part) ) {
-				return whole
-			}
-			
-			//it's a relative path from cssLocation, need to convert to
-			// prodLocation
-			var imagePath = steal.File(part).joinFrom( cssLoc),
-				fin = steal.File(imagePath).toReferenceFromSameDomain(prodLocation);
-			//print("  -> "+imagePath);
-			print("  "+part+" > "+fin);
-			return "url("+fin+")";
-		});
+			newCSss = css.replace(/url\(['"]?([^'"\)]*)['"]?\)/g, function( whole, part ) {
+
+				//check if url is relative
+				if (!isRelative(part) ) {
+					return whole
+				}
+
+				//it's a relative path from cssLocation, need to convert to
+				// prodLocation
+				var imagePath = steal.File(part).joinFrom(cssLoc),
+					fin = steal.File(imagePath).toReferenceFromSameDomain(prodLocation);
+				//print("  -> "+imagePath);
+				steal.print("  " + part + " > " + fin);
+				return "url(" + fin + ")";
+			});
 		return newCSss;
 	},
-	isRelative = function(part){
-		return !/^(http:\/\/|https:\/\/|\/\.\/|\.\.\/)/.test(part)
-	}
-	
-	var comments =  /\/\*.*?\*\//g,
-		newLines = /\n*/g,
-		space = /[ ]+/g,
-		spaceChars = /\s?([;:{},+>])\s?/g,
-		lastSemi = /;}/g;
-		
-	
-	steal.cssMin = function(css){
-		//remove comments
-		return css	.replace(comments,"")
-			.replace(newLines,"")
-			.replace(space," ")
-			.replace(spaceChars,'$1')
-			.replace(lastSemi,'}')
-	}
-	
+	isRelative = function( part ) {
+		// http://, https://, / 
+		return !/^(http:\/\/|https:\/\/|\/)/.test(part)
+	},
+    calcSavings = function(raw_len, minified_len) {
+        var diff_len = raw_len - minified_len, x = Math.pow(10,1);
+        return 'Compressed: '+(Math.round((diff_len/raw_len*100)*x)/x)+'%  Before: '+
+            string2size(raw_len)+'  After: '+string2size(minified_len);
+    },
+    string2size = function(bytes) {
+        var s = ['bytes','kb','mb','gb','tb','pb'];
+        var e = Math.floor(Math.log(bytes)/Math.log(1024));
+        return (bytes/Math.pow(1024,Math.floor(e))).toFixed(1)+' '+s[e];
+    };
 });
