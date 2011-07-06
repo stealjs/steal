a7dce9adff995522448ed94bbe7939984a69879f
Brian Moschel <brian.moschel@gmail.com>
2011-07-06 02:42:52 -0500

    styles builds

--- build/styles/styles.js
+++ build/styles/styles.js
@@ -73,4 +73,4 @@ steal(function( steal ) {
         var e = Math.floor(Math.log(bytes)/Math.log(1024));
         return (bytes/Math.pow(1024,Math.floor(e))).toFixed(1)+' '+s[e];
     };
-},'//steal/build/styles/cssmin');
+},'steal/build/styles/cssmin.js');
