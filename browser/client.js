6de9157e211cf202ec9ba40c10fa330f385181be
Brian Moschel <brian.moschel@gmail.com>
2011-08-09 03:43:48 -0400

    adding steal.browser and steal.client

---
+++ browser/client.js
@@ -0,0 +1,27 @@
+(function(){
+	// sometimes this might load without steal (in funcunit standalone mode)
+	if(typeof steal === "undefined"){
+		steal = {};
+	}
+	steal.client = {}
+	if (/browser=selenium/.test(window.location.search)) {
+		steal.client.dataQueue = []
+		steal.client.trigger = function(type, data){
+			steal.client.dataQueue.push({
+				type: type,
+				data: data
+			})
+		}
+	}
+	else if (/browser=jstestdriver/.test(window.location.search)) {
+		steal.client.trigger = function(type, data){
+			var dataString = JSON.stringify(data)
+			window.postMessage(dataString, "*")
+		}
+	}
+	else if (/browser=envjs/.test(window.location.search)) {
+		steal.client.trigger = function(type, data){
+			Envjs.browser.trigger(type, data)
+		}
+	}
+})()
