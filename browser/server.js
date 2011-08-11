6fe9057250bb1f880ddaf6955cbd4e65cfaf091d
Brian Moschel <brian.moschel@gmail.com>
2011-08-11 04:23:04 -0400

    working on phantom

---
+++ browser/server.js
@@ -0,0 +1,47 @@
+// listens for 3001, returns something super simple
+
+steal.browser.prototype.simpleServer = function(){
+	
+	var serv = new java.net.ServerSocket(3001);
+	
+	var sock = serv.accept();
+	var bufr = new java.io.BufferedReader(new java.io.InputStreamReader(sock.getInputStream()));
+	
+	var prtw = new java.io.PrintWriter(sock.getOutputStream(), false); // no autoFlush
+	var v = new java.util.Vector(10); // collects headers sent by browser
+	var done = false;
+	
+	while (!done) {
+		try {
+			var x = bufr.readLine();
+			if (x.length() == 0) {
+				done = true;
+			}
+			else {
+				var params = x.match(/^GET.*\?(.*)\s/)
+				if (params.length) {
+					DATA = params[1];
+//					print(params[1])
+				}
+				v.addElement(x);
+			}
+		} 
+		catch (e) {
+			done = true;
+		}
+	}
+	
+	prtw.println("HTTP/1.0 200 OK");
+	prtw.println("Content-type: text/plain");
+	prtw.println();
+	prtw.println("Here is your request...");
+	
+	prtw.flush();
+	
+	bufr.close();
+	prtw.close();
+	
+	sock.close();
+	serv.close();
+	arguments.callee();
+}
