882226badc7de79bf14e39289bf45d1957856c2f
Justin Meyer <justinbmeyer@gmail.com>
2011-08-18 11:50:12 -0500

    removed dummy

--- get/dummy.js
+++ get/dummy.js
@@ -1,7 +1,5 @@
 // a dummy version of steal that can run on a file and extract data from it ...
 steal(function(s){
-	print(s.toString());
-	return;
 	
 	s.dummy = function(code){
 		print("I SHOULD NOT BE RUNNING >>>!")
