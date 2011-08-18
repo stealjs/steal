de689238feb36c1c8f595f38b49732f938540156
Brian Moschel <brian.moschel@gmail.com>
2011-08-18 14:10:01 -0500

    loading with steal.production.js will work now from IE

--- steal.js
+++ steal.js
@@ -1853,7 +1853,7 @@ if (support.interactive) {
 	steal.after = after(steal.after, function(){
 		var interactive = getInteractiveScript();
 		// if no interactive script, this is a steal coming from inside a steal, let complete handle it
-		if (!interactive || !interactive.src || /steal\.js/.test(interactive.src)) {
+		if (!interactive || !interactive.src || /steal\.(production\.)*js/.test(interactive.src)) {
 			return;
 		}
 		var src = interactive.src;
