adc61801454e933592b38ba1151198861f30d338
Brian Moschel <brian.moschel@gmail.com>
2011-08-26 05:43:48 -0500

    adding some docs

--- browser/browser.js
+++ browser/browser.js
@@ -1,4 +1,25 @@
 (function(){
+	/**
+	 * steal.browser is an abstraction layer for browser drivers.  There is currently support for 
+	 * envjs, phantomjs, and selenium.  Each driver implements an API that includes open, bind, trigger, 
+	 * evaluate, and injectJS.  There is also a small client component that is loaded by steal.js.
+	 * 
+	 * The initial bootstrap of steal.browser works like this:
+	 * 1. steal.browser is used to open a page.  The page's URL is appended with 
+	 * ?mode=commandline&browser=phantomjs (or whatever driver is being used).
+	 * 2. On the client, steal.js does a check for these URL params.  If they're 
+	 * found, steal loads steal/browser/phantomjs/client.js.  Client.js is a small file that implements
+	 * methods used for evaluating code and triggering events.
+	 * 3. Client.js loads jquery.js, runs $.holdReady(true) (to prevent the app from initializing), and 
+	 * runs steal.client.trigger('clientloaded') (to notify the server that the browser's client.js has 
+	 * loaded.
+	 * 4. Now, the server can perform any startup logic it needs, like using injectJS to load files or 
+	 * evaluating code.  Whenever its done, it runs this.evaluate(function(){ $.holdReady(false); }).
+	 * 5. Document ready is fired on the client, and the application starts up.  Its important to note 
+	 * that any app using steal.browser can't trigger browser events until after document.ready.  This is 
+	 * required because the server needs to have a chance to load its own code and prevent race conditions.
+	 * @param {Object} options
+	 */
 	steal.browser = function(options){
 		this._events = {};
 		// driver should start the server if there is one (selenium/jstestdriver)
