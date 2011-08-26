b3736d8ad24b8885fe9fa5071efff23d63bae41b
Brian Moschel <brian.moschel@gmail.com>
2011-08-25 23:33:00 -0500

    making this a oneliner

--- steal.js
+++ steal.js
@@ -1892,15 +1892,7 @@ if (support.interactive) {
 }
 
 	// ===========  STEAL.BROWSERS ==========
-	if(win.location && /mode=commandline/.test(win.location.search)){
-		var browserMode = win.location.search.match(/browser=(\w+)/)[1],
-			clientPath =  "steal/browser/"+browserMode+"/client.js";
-		steal({
-			src: clientPath,
-			ignore: true
-		});
-	}
-	
+	win.location && /mode=commandline/.test(win.location.search) && steal("steal/browser/"+win.location.search.match(/browser=(\w+)/)[1]+"/client.js")
 	
 	// ===========  OPTIONS ==========
 	
