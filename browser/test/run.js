3e27248ecae4e95124aa02bc3d9bc0c4a4a37c65
Brian Moschel <brian.moschel@gmail.com>
2011-08-26 00:29:01 -0500

    reorganizing browsers

--- browser/test/run.js
+++ browser/test/run.js
@@ -36,27 +36,4 @@ steal('steal/test/test.js', function(s) {
 		browserTest(browsers[i])
 	}
 	
-//	s.test.test("phantomjs", function(){
-//		load('steal/rhino/rhino.js')
-//		steal("steal/browser/phantomjs", function(){
-//			var browser = new steal.browser.phantomjs({
-//				print: true
-//			});
-//			browser
-//				.bind('myevent', function(data){
-//					s.test.equals(data.foo, 'bar', 'bind works')
-//				})
-//				.bind('triggered', function(data){
-//					s.test.ok(true, 'injectJS works')
-//				})
-//				.open('steal/browser/test/mypage.html')
-//			var result = browser.evaluate(function(){
-//				return MyCo.foo;
-//			})
-//			s.test.equals(result, "bla", "execute works!")
-//			browser.injectJS('steal/browser/test/trigger.js')
-//			s.test.expect(3)
-//			browser.close();
-//		})
-//	})
 })
