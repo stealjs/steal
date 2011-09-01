2a3caf7c7bf1b89dd8ce4cc4d5dc4d6b0c22e4df
Brian Moschel <Brian.Moschel@contractor.cengage.com>
2011-08-31 23:51:36 -0400

    adding client

---
+++ html/client.js
@@ -0,0 +1,25 @@
+steal(function(){
+	steal.html = {};
+	var count = 0,
+		readyFunc;
+	/**
+	 * @function steal.html.wait
+	 * @parent steal.html
+	 * Waits for the html to finish
+	 */
+	steal.html.wait = function(){
+		count++;
+	};
+	/**
+	 * @function steal.html.ready
+	 * @parent steal.html
+	 * Lets the page know it's ready to render the html
+	 */
+	steal.html.ready = function(){
+		count--;
+		if(count <= 0){
+			steal.client.trigger("pageready")
+		}
+	};
+
+})
