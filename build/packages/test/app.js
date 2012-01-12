2712b93c99d99971e25cffb56ed781c86f135b37
Justin Meyer <justinbmeyer@gmail.com>
2012-01-12 16:08:21 -0600

    packages has depth, hard coded to 3

--- build/packages/test/app.js
+++ build/packages/test/app.js
@@ -1,5 +1,6 @@
 steal.packages('steal/build/packages/test/table_scroll.js',
-               'mxui/nav/accordion/accordion.js')
+               'mxui/nav/accordion/accordion.js',
+			   'mxui/layout/resize/resize.js')
 	.then('jquery/controller/route',
 		'jquery/view/ejs','./app.css')
 	.then('./ejs.ejs', function(){
@@ -14,8 +15,14 @@ $.Controller('Route', {
 		steal('mxui/nav/accordion', function(){
 			$('#accordion').mxui_nav_accordion()
 		})
+	},
+	"resize route" : function(){
+		steal('mxui/layout/resize', function(){
+			$('#resize').mxui_layout_resize()
+		})
 	}
-})
+});
+
 new Route(document.body);
 		
 		
