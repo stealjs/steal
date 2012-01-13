5d6579e73cc92f7ddba4d57605ce72337cb71eb0
Austin McDaniel <amcdaniel2@gmail.com>
2012-01-13 12:16:43 -0600

    Fix for stealing from a different domain and it thinking it was relative to the application
    Signed-off-by: Austin McDaniel <amcdaniel2@gmail.com>

--- steal.js
+++ steal.js
@@ -725,7 +725,7 @@
 			}
 			// only if we start with ./ or have a /foo should we join from cur
 			else if (this.isRelative() ) {
-				path = cur.join(path)
+				path = cur.join(this.domain() + path)
 			}
 			path.query = this.query;
 			return path;
