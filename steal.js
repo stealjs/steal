5be08bb0431125becb1e4a0e8e50652f2556209a
Brian Moschel <brian.moschel@gmail.com>
2012-07-06 09:57:11 -0500

    fixing CSS path so it doesn't load twice

--- steal.js
+++ steal.js
@@ -967,7 +967,7 @@
 		executed: function(name) {
 			// create the steal, mark it as loading, then as loaded
 			var stel = Resource.make( name );
-			stel.loading = true;
+			stel.loading = stel.executing = true;
 			//convert(stel, "complete");
 
 			steal.preexecuted(stel);
