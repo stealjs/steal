b074ce8f8f5c5bf3afd43ec7dfda2aa1be514a20
Austin McDaniel <amcdaniel2@gmail.com>
2013-01-16 15:07:53 -0600

    Adding better excluding capabilities

--- build/js/js_test.js
+++ build/js/js_test.js
@@ -60,4 +60,53 @@ steal('steal', 'steal/test', function(s) {
 			});
 	});
 
+	s.test.test("makePackage with excludes", function(){
+		load('steal/rhino/rhino.js');
+		steal('steal/build/js',
+			function(){
+				var res = steal.build.js.makePackage(
+				[
+					{
+						buildType : "js",
+						id : "a.js",
+						text: "a"
+					},
+					{
+						buildType : "js",
+						id : "b/b.js",
+						text: "b"
+					},
+					{
+						buildType : "css",
+						id : "c.css",
+						text: "c"
+					}
+				],
+				{
+					"package/1.js" : ["jquery/jquery.js"]
+				},
+				"package/css.css", { exclude: ['b/b'] })
+				
+				s.test.equals(
+					res.js,
+					// tell what this file has
+					'steal.has("a.js");'+
+					// steal any packages this package depends on
+					'steal({id:"package/1.js",waits:!0,has:["jquery/jquery.js"]});'+
+					'steal({id:"package/css.css",waits:!0,has:["c.css"]});'+
+					// clear pending for future steals
+					'steal.pushPending();'+
+					// the files and executed contexts
+					'a;steal.executed("a.js");'+
+					// pop the previous pending state into being so when this file completes, it's depeendencies will be executed
+					'steal.popPending();'+
+					'\n',
+					"js works");
+					
+				s.test.equals(res.css.code,"c")
+				
+				s.test.clear();
+			});
+	});
+
 });
