9ba4682c28e0917c24dc338b49daf862eb67546d
Manuel Mujica <evilsaurer@gmail.com>
2014-05-28 16:44:10 -0400

    Add forward slash plugin.
    'path/to/folder/' will be normalized to 'path/to/folder/folder'

---
+++ src/system-extension-forward-slash.js
@@ -0,0 +1,22 @@
+
+	// "path/to/folder/" -> "path/to/folder/folder"
+	var addForwardSlash = function(loader) {
+		var normalize = loader.normalize;
+
+		loader.normalize = function(name, parentName, parentAddress) {
+			var lastPos = name.length - 1,
+				secondToLast,
+				folderName;
+
+			if (name[lastPos] === "/") {
+				secondToLast = name.substring(0, lastPos).lastIndexOf("/");
+				folderName = name.substring(secondToLast + 1, lastPos);
+				name += folderName;
+			}
+			return normalize.call(this, name, parentName, parentAddress);
+		};
+	};
+
+	if (typeof System) {
+		addForwardSlash(System);
+	}
--- steal.js
+++ steal.js
@@ -3508,6 +3508,29 @@ var makeSteal = function(System){
 	}
 	
 
+
+	// "path/to/folder/" -> "path/to/folder/folder"
+	var addForwardSlash = function(loader) {
+		var normalize = loader.normalize;
+
+		loader.normalize = function(name, parentName, parentAddress) {
+			var lastPos = name.length - 1,
+				secondToLast,
+				folderName;
+
+			if (name[lastPos] === "/") {
+				secondToLast = name.substring(0, lastPos).lastIndexOf("/");
+				folderName = name.substring(secondToLast + 1, lastPos);
+				name += folderName;
+			}
+			return normalize.call(this, name, parentName, parentAddress);
+		};
+	};
+
+	if (typeof System) {
+		addForwardSlash(System);
+	}
+
 	var configData = {
 		env: "development"
 	};
