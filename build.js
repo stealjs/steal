6c2461f3119e93891ee0876b955113bb18690da5
Justin Meyer <justinbmeyer@gmail.com>
2014-07-15 11:11:37 -0500

    documentjs generated docs

---
+++ build.js
@@ -0,0 +1,12 @@
+var documentjs = require("documentjs"),
+	path = require("path");
+
+
+documentjs({
+	pattern: "{steal,steal-tools}/**/*.+(md)",
+	cwd: __dirname
+},{
+	out: path.join(__dirname,"docs"),
+	parent: "StealJS",
+	forceBuild: true
+});
