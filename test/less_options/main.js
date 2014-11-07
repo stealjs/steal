35fd39f0fbc51d4e1bf0e7ba45592413937ec0e8
Manuel Mujica <evilsaurer@gmail.com>
2014-11-07 10:07:15 -0300

    Fix test after rebase.

--- test/less_options/main.js
+++ test/less_options/main.js
@@ -4,7 +4,7 @@ if (typeof window !== "undefined" && window.QUnit) {
 
 	System.instantiate = function(load) {
 		if (load.name.indexOf("main.less") !== -1) {
-			var hasLineNumber = load.source.indexOf("line 1, input") !== -1,
+			var hasLineNumber = load.source.indexOf("line 1") !== -1,
 				hasStrictMath = load.source.indexOf("100%") !== -1;
 
 			QUnit.ok(hasLineNumber, "less set to dump line numbers");
