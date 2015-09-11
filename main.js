1bc6da3d52963881f1d8e22ca6a5c972d65c9d58
Matthew Phillips <matthew@matthewphillips.info>
2015-09-11 13:10:13 -0400

    Adds the trace extension
    This commit adds in the system-trace extension into core. This extension provides the ability to retain the source (and entire load object) for each module for faster reloading. It also provides APIs for traversing the dependency graph.

--- main.js
+++ main.js
@@ -266,6 +266,167 @@ var makeSteal = function(System){
 		addForwardSlash(System);
 	}
 
+function applyTraceExtension(loader){
+	if(loader._extensions) {
+		loader._extensions.push(applyTraceExtension);
+	}
+
+	loader._traceData = {
+		loads: {},
+		parentMap: {}
+	};
+
+	loader.getDependencies = function(moduleName){
+		return this.getModuleLoad(moduleName).metadata.dependencies;
+	};
+	loader.getDependants = function(moduleName){
+		var deps = [];
+		var pars = this._traceData.parentMap[moduleName] || {};
+		eachOf(pars, function(name) { deps.push(name); });
+		return deps;
+	};
+	loader.getModuleLoad = function(moduleName){
+		return this._traceData.loads[moduleName];
+	};
+	loader.getBundles = function(moduleName, visited){
+		visited = visited || {};
+		visited[moduleName] = true;
+		var loader = this;
+		var parentMap = loader._traceData.parentMap;
+		var parents = parentMap[moduleName];
+		if(!parents) return [moduleName];
+
+		var bundles = [];
+		eachOf(parents, function(parentName, value){
+			if(!visited[parentName])
+				bundles = bundles.concat(loader.getBundles(parentName, visited));
+		});
+		return bundles;
+	};
+	loader._allowModuleExecution = {};
+	loader.allowModuleExecution = function(name){
+		var loader = this;
+		return loader.normalize(name).then(function(name){
+			loader._allowModuleExecution[name] = true;
+		});
+	};
+
+	function eachOf(obj, callback){
+		var name, val;
+		for(name in obj) {
+			callback(name, obj[name]);
+		}
+	}
+
+	var normalize = loader.normalize;
+	loader.normalize = function(name, parentName){
+		var normalizePromise = normalize.apply(this, arguments);
+
+		if(parentName) {
+			var parentMap = this._traceData.parentMap;
+			return normalizePromise.then(function(name){
+				if(!parentMap[name]) {
+					parentMap[name] = {};
+				}
+				parentMap[name][parentName] = true;
+				return name;
+			});
+		}
+
+		return normalizePromise;
+	};
+
+	var emptyExecute = function(){
+		return loader.newModule({});
+	};
+
+
+	var passThroughModules = {
+		traceur: true,
+		babel: true
+	};
+	var isAllowedToExecute = function(load){
+		return passThroughModules[load.name] || this._allowModuleExecution[load.name];
+	};
+
+	var map = [].map || function(callback){
+		var res = [];
+		for(var i = 0, len = this.length; i < len; i++) {
+			res.push(callback(this[i]));
+		}
+		return res;
+	};
+
+	var transpiledDepsExp = /System\.register\((\[.+?\])\,/;
+	var singleQuoteExp = /'/g;
+	var instantiate = loader.instantiate;
+	loader.instantiate = function(load){
+		this._traceData.loads[load.name] = load;
+		var loader = this;
+		var instantiatePromise = Promise.resolve(instantiate.apply(this, arguments));
+
+		function finalizeResult(result){
+			var preventExecution = loader.preventModuleExecution &&
+				!isAllowedToExecute.call(loader, load);
+
+			// deps either comes from the instantiate result, or if an
+			// es6 module it was found in the transpile hook.
+			var deps = result ? result.deps : load.metadata.deps;
+
+			return Promise.all(map.call(deps, function(depName){
+				return loader.normalize(depName, load.name);
+			})).then(function(dependencies){
+				load.metadata.deps = deps;
+				load.metadata.dependencies = dependencies;
+
+				if(preventExecution) {
+					return {
+						deps: deps,
+						execute: emptyExecute
+					};
+				}
+
+				return result;
+
+			});
+		}
+
+		return instantiatePromise.then(function(result){
+			// This must be es6
+			if(!result) {
+				return loader.transpile(load).then(function(source){
+					load.metadata.transpiledSource = source;
+
+					var depsMatches = transpiledDepsExp.exec(source);
+					var depsSource = depsMatches ? depsMatches[1] : "[]";
+					var deps = JSON.parse(depsSource.replace(singleQuoteExp, '"'));
+					load.metadata.deps = deps;
+
+					return finalizeResult(result);
+				});
+			}
+			return finalizeResult(result);
+		});
+	};
+
+	var transpile = loader.transpile;
+	// Allow transpile to be memoized, but only once
+	loader.transpile = function(load){
+		var transpiled = load.metadata.transpiledSource;
+		if(transpiled) {
+			delete load.metadata.transpiledSource;
+			return Promise.resolve(transpiled);
+		}
+		return transpile.apply(this, arguments);
+	};
+}
+
+applyTraceExtension.name = "Trace";
+
+if(typeof System !== "undefined") {
+	applyTraceExtension(System);
+}
+
 	// Overwrites System.config with setter hooks
 	var setterConfig = function(loader, configSpecial){
 		var oldConfig = loader.config;
