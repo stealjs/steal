c4955e736e24bfdf468c98bb3df938b454519720
Kevin Phillips <kphillips86@gmail.com>
2016-03-04 12:32:52 -0600

    changes from running grunt build

--- ext/less.js
+++ ext/less.js
@@ -1,6 +1,6 @@
 var css = require("$css");
 var loader = require("@loader");
-var lessEngine = require("less");
+var lessEngine = require("@less-engine");
 
 exports.instantiate = css.instantiate;
 
@@ -30,6 +30,13 @@ exports.translate = function(load) {
 		}
 		renderOptions.paths = (options.paths || []).concat(pathParts.join('/'));
 
+		renderOptions.plugins = (options.plugins || []);
+		if (stealLessPlugin !== undefined) {
+			renderOptions.plugins.push(stealLessPlugin);
+		}
+
+		renderOptions.relativeUrls = options.relativeUrls === undefined ? true : options.relativeUrls;
+
 		var done = function(output) {
 			// Put the source map on metadata if one was created.
 			load.metadata.map = output.map;
@@ -43,5 +50,66 @@ exports.translate = function(load) {
 		lessEngine.render(load.source, renderOptions).then(done, fail);
 	});
 };
-exports.locateMacro = true;
+exports.locateScheme = true;
 exports.buildType = "css";
+
+// plugin to rewrite locate:// paths in imports
+var stealLessPlugin = undefined;
+if (lessEngine.FileManager) {
+	var FileManager = lessEngine.FileManager;
+
+	function StealLessManager() {
+		this.PATTERN = /locate:\/\/([a-z0-9/._@-]*)/ig;
+	}
+
+	StealLessManager.prototype = new FileManager();
+
+	StealLessManager.prototype.supports = function(filename) {
+		return true;
+	};
+
+	StealLessManager.prototype.locate = function(filename, currentDirectory) {
+		return Promise.resolve(loader.normalize(filename, currentDirectory))
+			.then(function(name){
+				return loader.locate({name: name, metadata: {}});
+			});
+	};
+
+	StealLessManager.prototype.loadFile = function(filename, currentDirectory, options, environment, callback) {
+		var self = this,
+			_callback = callback,
+			path = (currentDirectory + filename),
+			directory = path.substring(0, path.lastIndexOf('/')+1);
+
+		callback = function(err, file) {
+			var promises = [];
+			// collect locate promises
+			file.contents.replace(self.PATTERN, function (whole, path, index) {
+				promises.push(self.locate(path, file.filename.replace(loader.baseURL, '')).then(function(filename) {
+					return {
+						str: filename.replace(directory, ''),
+						loc: index,
+						del: whole.length
+					}
+				}));
+			});
+
+			Promise.all(promises).then(function(spliceDefs) {
+				for(var i = spliceDefs.length; i--;) {
+					var def = spliceDefs[i];
+					file.contents = file.contents.slice(0, def.loc) + def.str + file.contents.slice(def.loc + def.del);
+				}
+
+				_callback.apply(this, [err, file]);
+			});
+		};
+
+		FileManager.prototype.loadFile.call(this, filename, currentDirectory, options, environment, callback);
+	};
+
+	stealLessPlugin = {
+		install: function(less, pluginManager) {
+			pluginManager.addFileManager(new StealLessManager());
+		}
+	};
+}
