6d98cecf06c3bfeabaf916813b9297b6fec8f769
Matthew Phillips <matthew@matthewphillips.info>
2016-12-08 11:58:23 -0600

    Only append elements when running in the browser basics/basics

--- test/basics/basics.js
+++ test/basics/basics.js
@@ -1,5 +1,7 @@
 steal('basics/module', function(module){
 
+	var isNode = typeof process === "object" && {}.toString.call(process) === "[object process]";
+
 	if(typeof window !== "undefined" && window.QUnit) {
 		QUnit.ok(module, "got basics/module");
 		QUnit.equal(module.name, "module", "module name is right");
@@ -15,19 +17,21 @@ steal('basics/module', function(module){
 			console.log("basics loaded", module.name, module.es6module.name, module.es6module.amdModule.name);
 		}
 
-		var host = document.createElement("div");
-		var one = document.createElement("div");
-		var two = document.createElement("div");
-		var three = document.createElement("div");
+		if(!isNode) {
+			var host = document.createElement("div");
+			var one = document.createElement("div");
+			var two = document.createElement("div");
+			var three = document.createElement("div");
 
-		one.textContent = module.name;
-		two.textContent = module.es6module.name;
-		three.textContent = module.es6module.amdModule.name;
+			one.textContent = module.name;
+			two.textContent = module.es6module.name;
+			three.textContent = module.es6module.amdModule.name;
 
-		[one, two, three].forEach(function(div){
-			host.appendChild(div);
-		});
-		document.body.appendChild(host);
+			[one, two, three].forEach(function(div){
+				host.appendChild(div);
+			});
+			document.body.appendChild(host);
+		}
 	}
 
 });
