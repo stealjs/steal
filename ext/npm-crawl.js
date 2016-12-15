7a7b4a3e38ec32ae83f6d646b0c8d82f6e928575
Matthew Phillips <matthew@matthewphillips.info>
2016-12-15 16:34:17 -0500

    Fix progressively loaded plugins configuration
    This fixes https://github.com/stealjs/steal/issues/957
    
    When we progressively load a package we also load in `plugins`
    configuration it has. This configuration should be treated not as from
    the root package, because doing so has been causing it to be looked up
    from too deep of a folder. This closes #957

--- ext/npm-crawl.js
+++ ext/npm-crawl.js
@@ -93,7 +93,7 @@ var crawl = {
 				}
 			}
 
-			return crawl.loadPlugins(context, childPkg, isRoot, null,
+			return crawl.loadPlugins(context, childPkg, false, null,
 									skipSettingConfig).then(function(){
 				return localPkg;
 			});
