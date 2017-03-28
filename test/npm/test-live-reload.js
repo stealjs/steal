7d7df9c20af20c1f38527d52e8dde80548374d10
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-03-27 18:16:21 -0600

    Fix npm live reload tests

--- test/npm/test-live-reload.js
+++ test/npm/test-live-reload.js
@@ -1,19 +1,9 @@
+var makeIframe = require("../make_iframe");
+
 QUnit.module("live-reload behavior");
 
-
-var makeIframe = function(src){
-	var iframe = document.createElement('iframe');
-	window.removeMyself = function(){
-		delete window.removeMyself;
-		document.body.removeChild(iframe);
-		QUnit.start();
-	};
-	document.body.appendChild(iframe);
-	iframe.src = src;
-};
-
-asyncTest("can install something", function(){
-	makeIframe("live-reload/dev.html");
+QUnit.test("can install something", function(assert) {
+	makeIframe("live-reload/dev.html", assert);
 });
 
 QUnit.start();
