3f37c6a72a3c92b7c8ca6804d6881b3d775033bf
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-04-04 11:00:32 -0600

    Refactor tests to remove deprecated asyncTest method

--- src/json/test/test.js
+++ src/json/test/test.js
@@ -2,19 +2,21 @@ var QUnit = require("steal-qunit");
 
 QUnit.module("JSON support");
 
-QUnit.asyncTest("Basics works", function(assert){
-	steal.import("src/json/test/jsons/my.json").then(function(my){
-		assert.equal(my.name, "foo", "name is right");
-	}).then(start);
+QUnit.test("Basics works", function(assert){
+	return steal.import("src/json/test/jsons/my.json")
+		.then(function(my){
+			assert.equal(my.name, "foo", "name is right");
+		});
 });
 
-QUnit.asyncTest("Still resolves when we fail to parse", function(assert){
-	steal.import("src/json/test/jsons/bad.json").then(function(){
-		assert.ok(true);
-	}).then(start);
+QUnit.test("Still resolves when we fail to parse", function(assert){
+	return steal.import("src/json/test/jsons/bad.json")
+		.then(function(){
+			assert.ok(true);
+		});
 });
 
-QUnit.asyncTest("jsonOptions transform allows you to transform the json object", function(assert){
+QUnit.test("jsonOptions transform allows you to transform the json object", function(assert){
 	System.jsonOptions = {
 		transform: function(load, data){
 			delete data.priv;
@@ -22,9 +24,10 @@ QUnit.asyncTest("jsonOptions transform allows you to transform the json object",
 		}
 	};
 
-	steal.import("src/json/test/jsons/another.json").then(function(a){
-		assert.ok(!a.priv, "Private field excluded");
-	}).then(start);
+	return steal.import("src/json/test/jsons/another.json")
+		.then(function(a){
+			assert.ok(!a.priv, "Private field excluded");
+		});
 });
 
 QUnit.test("json extension should not parse css files", function(assert) {
