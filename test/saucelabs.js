566e75f8e08000b3df0c4686f155ff783cb7d11f
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-08-14 18:18:03 +0100

    Sync saucelab ios with master

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -29,9 +29,9 @@ var platforms = [{
 	version: '11.0'
 }, {
 	browserName: 'Safari',
-	'appium-version': '1.6.3',
+	'appium-version': '1.9.1',
 	platformName: 'iOS',
-	platformVersion: '10.0',
+	platformVersion: '10.3',
 	deviceName: 'iPhone 7 Simulator'
 }];
 
