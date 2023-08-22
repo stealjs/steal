37be5aba21ba2f14a95043fee3c29c3a0b4bcaee
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-23 00:22:24 +0530

    Update main.js

--- test/webworkers/main.js
+++ test/webworkers/main.js
@@ -1,8 +1,8 @@
-var workerURL = steal.config("stealURL") +
+let workerURL = steal.config("stealURL") +
 	"?main=webworkers/worker&config=" +
 	steal.config("configPath");
 
-var worker = new Worker(workerURL);
+let worker = new Worker(workerURL);
 
 worker.addEventListener("message", function(ev){
 	if(window.assert) {
