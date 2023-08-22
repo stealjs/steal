ccd1c3be702be2f3158cf89c44957fec31da83f5
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-22 19:17:34 +0530

    Update joinuris_test.js

--- test/joinuris_test.js
+++ test/joinuris_test.js
@@ -3,8 +3,8 @@
 QUnit.module("steal.joinURIs");
 
 QUnit.test("Handles URIs with an @ symbol", function(assert){
-	var expected;
-	var result;
+	let expected;
+	let result;
 
 	expected = "http://127.0.0.1:8080/images/bird.png";
 	result = steal.joinURIs("http://127.0.0.1:8080/node_modules/priv/dep/styles.less", "/images/bird.png");
